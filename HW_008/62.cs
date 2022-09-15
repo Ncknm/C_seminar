9e4aae7fb395f60d96968ff6e8af53296a039095
Ncknm <vladyko.nikita@gmail.com>
2022-09-16 00:04:31 +0500

    62 done!

--- HW_008/62.cs
+++ HW_008/62.cs
@@ -8,3 +8,58 @@ Console.WriteLine("HOMEWORK 8" + "\n");
 
 
 Console.WriteLine("\n" + "Задача 62." + "\n");
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] FillWithSpiral(int size)
+{
+    int elements = size * size;
+    int countElement = 0;
+    int begin = 0;
+    int[,] array = new int[size, size];
+    while (countElement < elements)
+    {
+        for (int i = begin; i < size; i++)
+        {
+            countElement++;
+            array[begin, i] = countElement;
+        }
+        for (int i = begin + 1; i < size; i++)
+        {
+            countElement++;
+            array[i, size - 1] = countElement;
+        }
+        for (int i = size - 2; i >= begin; i--)
+        {
+            countElement++;
+            array[size - 1, i] = countElement;
+        }
+        for (int i = size - 2; i >= begin + 1; i--)
+        {
+            countElement++;
+            array[i, begin] = countElement;
+        }
+        size -= 1;
+        begin += 1;
+    }
+    return array;
+}
+
+int[,] matrix = FillWithSpiral(Prompt("Введите размер квадратной матрицы: "));
+PrintMatrix(matrix);
