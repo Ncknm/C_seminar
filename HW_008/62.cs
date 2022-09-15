cac2bcdf309a4c3a563c627ff0ef736ba49d8162
Ncknm <vladyko.nikita@gmail.com>
2022-09-16 00:02:07 +0500

    Add 62.cs file

---
+++ HW_008/62.cs
@@ -0,0 +1,10 @@
+// HOMEWORK 8
+
+Console.Clear();
+Console.WriteLine("HOMEWORK 8" + "\n");
+
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+
+Console.WriteLine("\n" + "Задача 62." + "\n");
