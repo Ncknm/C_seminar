8bfea1553cf8eadd03c2902c23d37b61ebbe1810
Ncknm <vladyko.nikita@gmail.com>
2022-09-16 00:00:52 +0500

    60 done!

--- HW_008/60.cs
+++ HW_008/60.cs
@@ -7,6 +7,25 @@ Console.WriteLine("HOMEWORK 8" + "\n");
 // Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
 
-
-// Сделал сразу одинаковые размеры матриц, что бы можно было перемножать.
 Console.WriteLine("\n" + "Задача 60." + "\n");
+int line = 2;
+int columns = 2;
+int cube =2;
+int antiRepeat=1;
+int startTwoNum=10;
+Random rand = new Random();
+Console.WriteLine("Трехмерный массив: ");
+int[,,] matrix1 = new int[line, columns, cube];
+for (int i = 0; i < matrix1.GetLength(0); i++)  
+{
+    for (int j = 0; j < matrix1.GetLength(1); j++) 
+    {
+        for (int k = 0; k < matrix1.GetLength(2); k++)   
+        {
+            matrix1[i, j, k] = startTwoNum + antiRepeat;
+            antiRepeat++;
+            Console.Write($"{matrix1[i, j, k]} ({j},{k},{i})\t");   
+        }
+        Console.WriteLine();
+    }
+}
